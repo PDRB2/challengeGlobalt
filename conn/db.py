dc5af3774985ca94f9bf9cef61508464b6af8f6b
PDRB <rodriguezbaumann2@gmail.com>
2022-11-11 08:38:08 -0300

    Creacion de tablas Para insercion.

--- conn/db.py
+++ conn/db.py
@@ -15,19 +15,34 @@ def create_connection(db_file):
             conn.close()
 
 def initialize_data_base(formats, data, db):
-    conn = sqlite3.connect('../test_database')
+    conn = sqlite3.connect('../globalChallenge')
     c = conn.cursor()
 
     c.execute('''
-              CREATE TABLE IF NOT EXISTS products
-              ([product_id] INTEGER PRIMARY KEY, [product_name] TEXT)
+              CREATE TABLE IF NOT EXISTS hired_employees
+              ([id] INTEGER PRIMARY KEY --comment 'id of the employee'
+              ,[name] STRING    -- comment 'Name and surname of the employee'
+              ,[datetime] STRING --comment 'Hire datetime in ISO format'
+              ,[department_id] INTEGER --comment 'Id of the department which the employee was hired for'
+              ,       [job_id] INTEGER --comment 'Id of the job which the employee was hired for'
+              )
               ''')
 
     c.execute('''
+                 CREATE TABLE IF NOT EXISTS departments
+                 ([id] INTEGER PRIMARY KEY --comment 'id of the department'
+                 ,   [department] STRING --comment 'Name  of the department'
+                 )
+                 ''')
+
+    c.execute('''
               CREATE TABLE IF NOT EXISTS prices
-              ([product_id] INTEGER PRIMARY KEY, [price] INTEGER)
+              ([id] INTEGER PRIMARY KEY --comment 'id of the department'
+              , [job] INTEGER --comment 'Name  of the job'
+              )
               ''')
 
     conn.commit()
-    c.execute('''select * from products''')
+    c.execute('''select * from prices''')
+    print(c.fetchall())
     print('llegue a incializar')
